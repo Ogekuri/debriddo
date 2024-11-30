ccd79b17af809f180a75f402748903fbe24099a0
Francesco Rolando <francesco.rolando@datalogic.com>
2024-11-30 14:10:28 +0100

    v0.0.10: Rilascio della versione v0.0.10

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Application name
 APPLICATION_NAME = "Debriddo"
-APPLICATION_VERSION = "0.0.9"
+APPLICATION_VERSION = "0.0.10"
 APPLICATION_DESCRIPTION = "Stremio's addon to online search with debrid support."
 
 # SQL3llite database
