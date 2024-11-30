7179f32b678e37dcf8f85bd407ab86bf446e92f1
Francesco Rolando <francesco.rolando@datalogic.com>
2024-11-30 12:39:33 +0100

    v0.0.8: Rilascio della versione v0.0.8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Usa valori di default se non sono stati definiti
 # per default in ascolto sul container docker
@@ -8,4 +8,6 @@ CFG_NODE_URL=${DOCKER_URL:-http://127.0.0.1:8000}
 CFG_NODE_ENV=${DOCKER_ENV:-}
 
 echo -e "INFO:     ENTRYPOINT: "NODE_URL=$CFG_NODE_URL NODE_ENV=$CFG_NODE_ENV exec uvicorn "main:app" --reload --host $CFG_NODE_HOST --port $CFG_NODE_PORT
-NODE_URL=$CFG_NODE_URL NODE_ENV=$CFG_NODE_ENV exec uvicorn "main:app" --reload --host $CFG_NODE_HOST --port $CFG_NODE_PORT
+export NODE_URL=$CFG_NODE_URL
+export NODE_ENV=$CFG_NODE_ENV
+exec uvicorn "main:app" --reload --host $CFG_NODE_HOST --port $CFG_NODE_PORT
