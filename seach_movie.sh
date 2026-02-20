7c2ee181c69a9ff68d27a28fb1c69b11f26a090f
Francesco Rolando <ogekuri@gmail.com>
2026-02-20 15:55:37 +0100

    new(core): add seach_movie.sh and seach_series.sh scripts.

---
+++ seach_movie.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./api_tester_ita-only.sh search --stream-type movie --stream-id tt0133093
---
+++ seach_series.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./api_tester_ita-only.sh search --stream-type series --stream-id tt1839578:3:1
+
